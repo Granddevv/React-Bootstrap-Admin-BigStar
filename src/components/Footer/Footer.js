f8f675b721042bd5cda23a19e197bc675df8ff0f
granddevv <spacingflight@outlook.com>
2018-01-05 10:25:15 +0800

    Add Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="app-footer">
+        <span><a href="https://genesisui.com">Clever</a> &copy; 2018 creativeLabs.</span>
+        <span className="ml-auto">Powered by <a href="https://genesisui.com">GenesisUI</a></span>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
